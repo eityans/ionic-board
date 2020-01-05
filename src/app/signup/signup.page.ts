c217321b228b6899b7571de2e3e4c4da96fa6d15
eityans <eityans@gmail.com>
2020-01-05 15:17:18 +0900

    sign up

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+//Firebase
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +11,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  signup: {
+    email: string,
+    password: string,
+    name: string
+  } = {
+    email: '',
+    password: '',
+    name: ''
+  };
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private toastCtrl: ToastController,
+    private afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit() {
   }
 
+  signUp() {
+    this.afAuth.auth
+      .createUserWithEmailAndPassword(this.signup.email, this.signup.password)
+      .then(created => {
+        const newUser = created.user;
+        newUser.updateProfile({
+          displayName: this.signup.name,
+          photoURL: ''
+        })
+        .then(async () => {
+          const toast = await this.toastCtrl.create({
+            message: `${created.user.displayName}さんを登録しました`,
+            duration: 3000
+          });
+          await toast.present();
+        });
+        this.goBack();
+      })
+      .catch(async error => {
+        const toast = await this.toastCtrl.create({
+          message: error.toString(),
+          duration: 3000
+        });
+        await toast.present();
+      });
+  }
+
+  goBack() {
+    this.router.navigate(['/login']);
+  }
 }
